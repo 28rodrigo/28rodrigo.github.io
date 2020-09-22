44739a3022da0cb163a7cea56590cb1d06193f01
28rodrigo <28rodrigopereira@gmail.com>
2020-09-22 19:24:35 +0100

    mobile updates

---
+++ precache-manifest.ca00ffd9806ff75ab00687248c647cde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "626e5caec50107d0d06801cb612b3b06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1dafea5b72da1a846671",
+    "url": "/static/css/main.cd1b8d3d.chunk.css"
+  },
+  {
+    "revision": "38f014c523bfe0de036e",
+    "url": "/static/js/2.a016f79c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a016f79c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dafea5b72da1a846671",
+    "url": "/static/js/main.5d81764d.chunk.js"
+  },
+  {
+    "revision": "030f424d19fe62695994",
+    "url": "/static/js/runtime-main.da7c8a68.js"
+  }
+]);
